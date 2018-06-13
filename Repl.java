0a6a8f648745654e3194c6faab505faa7a6a8200
Vladislav Kostylev <kostylev.v@gmail.com>
2018-06-13 23:33:57 +0300

    The sum of two integers

---
+++ Repl.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Repl {
+  public static void main(String[] args) {
+      while(true) {
+        read();
+        if  (!eval()) break;
+      }
+      System.out.println("Bye!");
+  }
+
+  private static boolean eval() {
+      switch (operator) {
+        case "/exit": return false;
+
+        default:
+        if (operands.size() >= 1) System.out.println(sum());
+      }
+    return true;
+  }
+
+  private static void read() {
+    Scanner sc = new Scanner(System.in);
+    String line = sc.nextLine();
+    String[] words = line.split(DELIM);
+    operator = "";
+    operands = new ArrayList<Integer>();
+
+    for (String word : words) {
+      try {
+        int operand = Integer.parseInt(word);
+        operands.add(operand);
+
+      } catch (NumberFormatException nfe) {
+        operator = word;
+      }
+    }
+  }
+
+  private static int sum() {
+    int result = 0;
+    for (Integer i : operands) result += i;
+    return result;
+  }
+
+  private static final String TERMINATE = "/exit";
+  private static final String DELIM = " ";
+  private static String operator;
+  private static List<Integer> operands;
+
+}
