5875c81c76209e4e93ad455b4b67f0f1e192fc30
Anastasia Miroshnikova <miffka1992@inbox.ru>
2018-10-02 10:09:37 +0300

    Stage 5 completed

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     }
 
     public static boolean isValidExpression(String input) {
-        return input.matches("[\\d\\+\\-\\s]+");
+        return input.matches("[\\d+\\- ]+");
     }
 
     public static boolean answerToServiceCommand(String input){
@@ -19,6 +19,8 @@ public class Main {
         } else if (input.equals("/help")) {
             System.out.println("The program serves as microcalculator");
 
+        } else {
+            System.out.println("Unknown command");
         }
         return stop_iteration;
     }
@@ -30,21 +32,32 @@ public class Main {
         int n_minuses = 0;
         boolean next_minus = false;
         for (int i = 0; i < numbers_and_operators.length; i++) {
-            if (numbers_and_operators[i].matches("-?\\d+")) {
-                number = Integer.parseInt(numbers_and_operators[i]);
-                if (next_minus) {
-                    ans -= number;
-                } else {
-                    ans += number;
+            if (i % 2 == 0) {
+                try {
+                    number = Integer.parseInt(numbers_and_operators[i]);
+                    if (next_minus) {
+                        ans -= number;
+                    } else {
+                        ans += number;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid expression");
+                    break;
                 }
-            } else if (numbers_and_operators[i].matches("[+\\-]+")) {
-                n_minuses = numbers_and_operators[i].replaceAll("[^\\-]", "").length();
-                next_minus = n_minuses % 2 == 1;
             } else {
-                System.out.println("Wrong input");
+                if (numbers_and_operators[i].matches("[+\\-]+")) {
+                    n_minuses = numbers_and_operators[i].replaceAll("[^\\-]", "").length();
+                    next_minus = n_minuses % 2 == 1;
+                } else {
+                    System.out.println("Invalid expression");
+                    break;
+                }
+            }
+
+            if (i == numbers_and_operators.length - 1) {
+                System.out.println(ans);
             }
         }
-        System.out.println(ans);
     }
 
     public static void main(String[] args) {
@@ -63,7 +76,7 @@ public class Main {
                     calculateAndPrintExpression(str);
 
                 } else { // Invalid string input
-                    System.out.println(str);
+                    System.out.println("Invalid expression");
                 }
             }
         }
