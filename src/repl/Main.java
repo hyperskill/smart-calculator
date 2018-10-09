de4a553e834f9b9754cc0cdefbddaa20578f0c16
whitelext <whitelext@gmail.com>
2018-10-10 01:37:50 +0300

    ez

--- src/repl/Main.java
+++ src/repl/Main.java
@@ -1,7 +1,22 @@
 package repl;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        Scanner a = new Scanner(System.in);
+        int s = 0;
+        for(int i =0;i<2;i++)
+        {
+            String p = a.next();
+            if (p.equals("/exit"))
+            {
+                System.out.println("Bye!");
+                return;
+            }
+            int u = Integer.parseInt(p);
+            s+=u;
+
+
+        }
+        System.out.print(s);
     }
 }
