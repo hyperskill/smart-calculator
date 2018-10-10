5cacdbc5dbe60cd219a98682788ea05fa91f71c2
whitelext <whitelext@gmail.com>
2018-10-10 22:44:00 +0300

    ez

--- src/repl/Main.java
+++ src/repl/Main.java
@@ -4,18 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
         int s = 0;
-        for(int i =0;i<2;i++)
-        {
-            String p = a.next();
-            if (p.equals("/exit"))
-            {
+        if (a.hasNext("/help")){
+                System.out.println("The program calculates the sum of numbers");
+                String y = a.nextLine();
+        }
+
+        if (a.hasNext("/exit")) {
                 System.out.println("Bye!");
                 return;
-            }
-            int u = Integer.parseInt(p);
+        }
+
+        String l = a.nextLine();
+        l = l.replaceAll("\\s","");
+        char o[]=l.toCharArray();
+        for (char r:o) {
+            int u = Character.getNumericValue(r);
             s+=u;
-
-
         }
         System.out.print(s);
     }
