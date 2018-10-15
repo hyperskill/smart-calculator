e4b60875507ee1fad72e66a3e944a5370411ffa2
whitelext <whitelext@gmail.com>
2018-10-15 23:00:09 +0300

    Stage 4 changes

--- src/repl/Main.java
+++ src/repl/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
         int s = 0;
-        if (a.hasNext("/help")){
-            System.out.println("The program calculates the sum of numbers");
+        if (a.hasNext("/help")) {
+            System.out.println("The program сan add and sub numbers, -- = +");
             String y = a.nextLine();
         }
 
@@ -15,12 +15,29 @@ public class Main {
         }
 
         String l = a.nextLine();
-        String[] num = l.split(" ");
-        for (String n:num) {
-            int u = Integer.parseInt(n);
-            s+=u;
+        String[] num = l.split("\\s+");
+        int k = 2;
+        int sign = 1;
+        for (String n : num) {
+            if (k % 2 == 0) {
+                int u = Integer.parseInt(n);
+                s += u * sign;
+            }
+            else {
+                if (n.charAt(0)=='-') {
+                    if (n.length() % 2 == 1)
+                        sign = -1;
+                    else
+                        sign = 1;
+                }
+                else
+                    sign = 1;
+
+            }
+            k++;
+
         }
         System.out.print(s);
-    }
 
+    }
 }
