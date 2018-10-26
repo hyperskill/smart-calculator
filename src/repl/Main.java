aeb4283f96f1fc6b6563bcbbed394c33bb0a7f14
roman <shilov-rom@mail.ru>
2018-10-26 14:22:46 +0300

    stage 2 done!

--- src/repl/Main.java
+++ src/repl/Main.java
@@ -1,7 +1,44 @@
 package repl;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int sum, endNumber, beginNumber, count;
+        int[] arr = new int[2];
+
+        String s;
+
+        while(true)
+        {
+            arr[0] = 0;
+            arr[1] = 0;
+            beginNumber = 0;
+            count = 0;
+            s = in.nextLine();
+            if(s.equals("/exit"))
+            {
+                System.out.println("Bye!");
+                break;
+            }
+            if (!s.isEmpty())
+            {
+                for(int i = 0; i < s.length(); i++)
+                {
+                    if (s.charAt(i) == ' '){
+                        endNumber = i;
+                        arr[count] = Integer.parseInt(s.substring(beginNumber, endNumber));
+                        count++;
+                        beginNumber = endNumber + 1;
+                    }
+                    else if (i == s.length()-1){
+                        arr[count] = Integer.parseInt(s.substring(beginNumber, s.length()));
+                    }
+                }
+                sum = arr[0] + arr[1];
+                System.out.println(sum);
+            }
+        }
     }
 }
