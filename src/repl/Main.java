4c0c63cdbcc1e5bb04c6b25e1469e3acd2f28b10
roman <shilov-rom@mail.ru>
2018-10-31 14:26:01 +0300

    stage 4 done!

--- src/repl/Main.java
+++ src/repl/Main.java
@@ -6,45 +6,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int sum, endNumber, beginNumber, count;
+        int sum;
         String s;
 
         while(true)
         {
-            beginNumber = 0;
-            count = 0;
+            sum = 0;
             s = in.nextLine();
-            String[] arrS = s.split(" ");
-            int[] arr = new int[arrS.length + 1]; //if only 1 number was printed without space, we need at list 1 element in array
+
+            String s1 = s.replaceAll("\\s+", "");
+            String s2 = s1.replaceAll("[-]{2}", "+");
+            s1 = s2.replaceAll("[-]","+-");
+            String[] arrS = s1.split("[+]+");
+
             if(s.equals("/exit"))
             {
                 System.out.println("Bye!");
                 break;
             }
-            if(s.equals("/help"))
-            {
-                System.out.println("The program calculates the sum of numbers");
+            if(s.equals("/help")) {
+                System.out.println("The program calculates the sum of numbers if you print addition operator \"+\" \n" +
+                        "The program calculates the subtraction of numbers if you print subtraction operator \"-\"");
             }
-            else if (!s.isEmpty())
-            {
-                sum = 0;
-                for(int i = 0; i < s.length(); i++)
-                {
-                    if (s.charAt(i) == ' '){
-                        endNumber = i;
-                        arr[count] = Integer.parseInt(s.substring(beginNumber, endNumber));
-                        count++;
-                        beginNumber = endNumber + 1;
-                    }
-                    else if (i == s.length()-1){
-                        arr[count] = Integer.parseInt(s.substring(beginNumber, s.length()));
-                    }
-                }
-                for (int i = 0; i< arr.length; i++)
-                    sum += arr[i];
 
-                System.out.println(sum);
+            for (int i = 0; i< arrS.length; i++){
+                if(arrS[i].equals(""))
+                    sum += 0;
+                else
+                    sum += Integer.parseInt(arrS[i]);
             }
+            System.out.println(sum);
         }
     }
 }
