f19989e448ed5d5579a090605eb75c84f3681b57
Vlad <vlad.mihai28@yahoo.com>
2018-11-01 16:47:17 +0000

    Stage 2

--- src/repl/Main.java
+++ src/repl/Main.java
@@ -1,7 +1,30 @@
 package repl;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
+
+  private static boolean running = true;
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String input;
+    while(running) {
+      input = in.nextLine();
+      if(input.length() == 0) continue;
+      if(input.equals("/exit")) {
+        running = false;
+        System.out.println("Bye!");
+        continue;
+      }
+      String[] commands = input.split("\\s");
+      if(commands.length == 1) {
+        System.out.println(Integer.parseInt(commands[0]));
+      }
+      else {
+        int a = Integer.parseInt(commands[0]);
+        int b = Integer.parseInt(commands[1]);
+        System.out.println(a + b);
+      }
     }
+  }
 }
