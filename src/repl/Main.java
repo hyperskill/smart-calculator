78295ecafccec9b64e99a709f3cf03dbcbd3da42
Oleksii Petenko <44145167+oleksiipet@users.noreply.github.com>
2018-11-03 00:10:01 +0200

    Implemented second stage

--- src/repl/Main.java
+++ src/repl/Main.java
@@ -1,7 +1,23 @@
 package repl;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
+
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      while (true) {
+        String line = sc.nextLine();
+        if (line.isEmpty()) {
+          continue;
+        }
+        if (line.equals("/exit")) {
+          System.out.printf("Bye!\n");
+          break;
+        }
+        String[] terms = line.split("\\s+");
+        System.out.printf("%d\n", Integer.parseInt(terms[0]) + Integer.parseInt(terms[1]));
+      }
     }
+  }
 }
