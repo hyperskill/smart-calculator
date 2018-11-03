98247904b154b6c70959601bd2621e5754f22322
Oleksii Petenko <44145167+oleksiipet@users.noreply.github.com>
2018-11-03 14:26:55 +0200

    part 3 implemented

--- src/repl/Main.java
+++ src/repl/Main.java
@@ -1,5 +1,6 @@
 package repl;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +9,10 @@ public class Main {
     try (Scanner sc = new Scanner(System.in)) {
       while (true) {
         String line = sc.nextLine();
+        if (line.equals("/help")) {
+          System.out.printf("The program calculates the sum of numbers\n");
+          continue;
+        }
         if (line.isEmpty()) {
           continue;
         }
@@ -16,8 +21,10 @@ public class Main {
           break;
         }
         String[] terms = line.split("\\s+");
-        System.out.printf("%d\n", Integer.parseInt(terms[0]) + Integer.parseInt(terms[1]));
+        int[] numbers = Arrays.stream(terms).mapToInt(Integer::parseInt).toArray();
+        System.out.printf("%d\n", Arrays.stream(numbers).sum());
       }
     }
   }
 }
+
