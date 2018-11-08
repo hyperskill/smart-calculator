c08b7fe85b7f68c01468d6d24b6a1e3fb50c6ff5
roman <shilov-rom@mail.ru>
2018-11-08 18:45:32 +0300

    stage 7 done!

--- src/repl/Main.java
+++ src/repl/Main.java
@@ -1,112 +1,195 @@
 package repl;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Pattern;
 
+
 public class Main {
+    static Map<String, String> inputs = new HashMap<String, String>();
+    static Deque<String> postfixStack = new ArrayDeque<String>();
+    static Deque<Integer> resStack = new ArrayDeque<Integer>();
+    static Deque<Character> operStack = new ArrayDeque<Character>();
 
-    public static int inputChecker(String str) {
-        Pattern id = Pattern.compile("[a-zA-z]+=.*");
-        Pattern val = Pattern.compile("[a-zA-Z]+=[0-9]+=?.*");
-        Pattern assign = Pattern.compile("[a-zA-Z]+(=[0-9]+)");
-        Pattern equal = Pattern.compile("[=]");
+    static Pattern digits = Pattern.compile("\\d+");
+    static Pattern opers = Pattern.compile("[+-/*/()]");
 
-        if(equal.matcher(str).find()) {
-            if (!id.matcher(str).matches()) {
-                System.out.println("Invalid identifier");
-            } else if (!val.matcher(str).matches()) {
-                System.out.println("Invalid value");
-            } else if (!assign.matcher(str).matches()) {
-                System.out.println("Invalid assignment");
-            } else
-                return 1;
-
-            return -1;
-        }
-        return 1;
+    public static String spaceKiller(String s){
+        s = s.replaceAll(" ", "");//kill all spaces
+        s = s.replaceAll("[-]{2}","+");//double minus to plus
+        s = s.replaceAll("[+][-]", "-");//if number of minuses is odd stays only 1 minus
+        return s.replaceAll("[+]+","+");//if multiple pluses stays only 1 plus
     }
+
+    public static int commandChecker(String s){
+        if(s.equals("/exit"))
+        {
+            System.out.println("Bye!");
+            return -1;   //quit the program
+        }
+        if(s.equals("/help")) {
+            System.out.println("The program supports multiplication \"*\", integer division \"/\", \n" +
+                    "addition \"+\", subtraction \"-\" and parentheses. Also, variables can be used, for instance: \n" +
+                    "a = 1 \nb = 3\na + b\n");
+            return 1;//print help message and continue
+        }
+        return 0; //parse the input
+    }
+
+    public static int assignChecker(String s){
+        Pattern id = Pattern.compile("[a-zA-z]+=.*");
+        Pattern val = Pattern.compile("[a-zA-Z]+=[0-9]+=?[\\d]*");
+        Pattern assign = Pattern.compile("[a-zA-Z]+(=[0-9]+)");
+
+        if (!id.matcher(s).matches()) {
+            System.out.println("Invalid identifier");
+            return 1;
+        } else if ((!val.matcher(s).matches()) && !inputs.containsKey(s.split("[=]")[1])) {
+            System.out.println("Invalid value");
+            return 1;
+        } else if (!assign.matcher(s).matches()) {
+            System.out.println("Invalid assignment");
+            return 1;
+        }
+        return 0;
+    }
+
+    public static int putVarToMap(String s){
+        String varName = s.split("[=]")[0];
+        if(digits.matcher(s.split("[=]")[1]).matches()) {
+            String varValue = s.split("[=]")[1];
+            inputs.put(varName, varValue);
+        }else if(inputs.containsKey(s.split("[=]")[1])){
+            inputs.put(varName, inputs.get(s.split("[=]")[1]));
+        }else{
+            System.out.println("Unknown variable");
+            return 1;
+        }
+        return 0;
+    }
+
+
+
+    public static int formRPNStack(char[] operators){
+        String s = "";
+        for(int i = 0; i < operators.length; i++){
+
+            if (inputs.containsKey(Character.toString(operators[i]))) {
+                postfixStack.push(inputs.get(Character.toString(operators[i])));
+                continue;
+
+            } else if (digits.matcher(Character.toString(operators[i])).matches()) {
+                if(i > 0 && !digits.matcher(Character.toString(operators[i-1])).matches())
+                    s = Character.toString(operators[i]);
+                else if(i == 0)
+                    s = Character.toString(operators[i]);
+
+                if(i < operators.length-1 && digits.matcher(Character.toString(operators[i+1])).matches()){
+                    s = s + Character.toString(operators[i+1]);
+                }
+                else {
+                    postfixStack.push(s);
+                }
+                continue;
+            } else if(!opers.matcher(Character.toString(operators[i])).matches()){
+                System.out.println("Unknown variable");
+                return 1;
+            }
+
+            if (operStack.isEmpty() || operStack.peek() == '(' || operators[i] == '(' ||
+                    (operators[i]=='*' || operators[i] == '/') && (operStack.peek() == '+' || operStack.peek() == '-'))
+            {
+                operStack.push(operators[i]);
+            } else if (((((operators[i]=='+') || operators[i]=='-') &&
+                    (operStack.peek() == '+' || operStack.peek() == '-')) ||
+                    operStack.peek() =='*' || operStack.peek() =='/') && operators[i]!=')') {
+                while (!operStack.isEmpty() && ((((operators[i]=='+') || operators[i]=='-') &&
+                        (operStack.peek() == '+' || operStack.peek() == '-')) ||
+                        operStack.peek() =='*' || operStack.peek() =='/')) {
+                    postfixStack.push(operStack.pop().toString());
+                }
+                operStack.push(operators[i]);
+            } else if (operators[i] == ')') {
+                while (operStack.peek() != '(')
+                 //   if(operStack.isEmpty())
+               //     {
+                //        break;
+                //    }
+                    postfixStack.push(operStack.pop().toString());
+                if (operStack.peek() == '(') {
+                    operStack.pop();
+                }
+            } else {
+                operStack.push(operators[i]);
+            }
+        }
+        while (!operStack.isEmpty()){
+            if(operStack.peek() == '(' || operStack.peek() == ')'){
+                System.out.println("Invalid expression");
+                return 1;
+            }
+            postfixStack.push(operStack.pop().toString());
+        }
+        return 0;
+    }
+
+    public static void calcResult(){
+        ////calculation of postfix //////////////////////
+        while (!postfixStack.isEmpty()){
+            if(digits.matcher(postfixStack.peekLast()).matches()){
+                resStack.push(Integer.parseInt(postfixStack.pollLast()));
+            }else if(opers.matcher(postfixStack.peekFirst()).find()){
+                int a = resStack.pollFirst();
+                int b = resStack.pollFirst();
+                switch (postfixStack.pollLast().charAt(0)){
+                    case '+':
+                        resStack.addFirst(a + b);
+                        break;
+                    case '-':
+                        resStack.addFirst(b - a);
+                        break;
+                    case '*':
+                        resStack.addFirst(a * b);
+                        break;
+                    case '/':
+                        resStack.addFirst(a / b);
+                }
+            }
+        }
+        System.out.println(resStack.peekFirst());
+    }
+
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Map<String, Integer> inputs = new HashMap<String, Integer>();
-        int sum = 0;
+        int flag;
         String s;
-        Pattern plusMinus = Pattern.compile("[+-]");
-        Pattern equal = Pattern.compile("[=]");
-        Pattern digits = Pattern.compile("\\d+");
-        Pattern id = Pattern.compile("[a-zA-z]+=.*");
-        Pattern val = Pattern.compile("[a-zA-Z]+=[0-9]+=?.*");
-        Pattern assign = Pattern.compile("[a-zA-Z]+(=[0-9]+)");
 
-        while(true)
-        {
+        while(true) {
             s = in.nextLine();
-            s = s.replaceAll(" ", "");
-
-            if(s.equals("/exit"))
-            {
-                System.out.println("Bye!");
+            s = spaceKiller(s);//remove all no-needed characters
+            flag = commandChecker(s);//check whether string equals some command
+            if(flag == -1)
                 break;
-            }
-            if(s.equals("/help")) {
-                System.out.println("The program calculates the sum of numbers if you print addition operator \"+\" \n" +
-                        "The program calculates the subtraction of numbers if you print subtraction operator \"-\"");
+            else if(flag == 1)
                 continue;
-            }
 
-            if(equal.matcher(s).find()) {
-                if (!id.matcher(s).matches()) {
-                    System.out.println("Invalid identifier");
+            resStack.clear();
+            postfixStack.clear();
+            operStack.clear();
+            if(s.contains("=")) {
+                flag = assignChecker(s);//check whether assignment is correct
+                if (flag == 1)
                     continue;
-                } else if ((!val.matcher(s).matches()) && !inputs.containsKey(s.split("[=]")[1])) {
-                    System.out.println("Invalid value");
+                flag = putVarToMap(s);//put vars to map
+                if (flag == 1)
                     continue;
-                } else if (!assign.matcher(s).matches()) {
-                    System.out.println("Invalid assignment");
-                    continue;
-                }
-            }
+            }else{ //printed string does not contains "=", therefore it is might be an expression
 
-            if(!equal.matcher(s).find()){//plusMinus.matcher(s).find()) {
-                sum = 0;
-                String[] vars = s.split("[+-]");
-                String[] signs = s.split("([a-zA-Z]+)|([0-9]+)");
-                for(int i = 0; i < vars.length; i++){
-                    if(inputs.containsKey(vars[i])){
-                        if(plusMinus.matcher(s).matches()){
-                            if(signs[i].equals("-")) {
-                                sum -= inputs.get(vars[i]);
-                            }else {
-                                sum += inputs.get(vars[i]);
-                            }
-                        }else
-                            sum += inputs.get(vars[i]);
-                    }else if(digits.matcher(vars[i]).matches()){
-                        if(plusMinus.matcher(s).matches()) {
-                            if (signs[i].equals("-")) {
-                                sum -= Integer.parseInt(vars[i]);
-                            } else {
-                                sum += Integer.parseInt(vars[i]);
-                            }
-                        }else
-                            sum += Integer.parseInt(vars[i]);
-                    }else{
-                        System.out.println("Unknown variable");
-                    }
-                }
-                System.out.println(sum);
-            }else {
-                String varName = s.split("[=]")[0];
-                if(digits.matcher(s.split("[=]")[1]).matches()) {
-                    Integer varValue = Integer.parseInt(s.split("[=]")[1]);
-                    inputs.put(varName, varValue);
-                }else if(inputs.containsKey(s.split("[=]")[1])){
-                    inputs.put(varName, inputs.get(s.split("[=]")[1]));
-                }else{
-                    System.out.println("Unknown variable");
-                }
+                flag = formRPNStack(s.toCharArray());
+                if (flag == 1)
+                    continue;
+
+                calcResult();
             }
         }
     }
