bd9f90f057ecb9064e92cb851f1f50bb6b1438a6
dspatched <dudkin.iluha@gmail.com>
2018-11-13 18:51:12 +0300

    Solution to a given problem

--- src/repl/Main.java
+++ src/repl/Main.java
@@ -1,7 +1,29 @@
 package repl;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String line = scanner.nextLine();
+            int n = 0;
+            int m = 0;
+            String first = "";
+            String second = "";
+            boolean flag = false;
+            if (line.equals("/exit")) { System.out.println("Bye!"); break; }
+            if (line.trim().equals("")) { continue; }
+            else {
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == ' ') { flag = true; continue; }
+                    if (!flag) first += line.charAt(i);
+                    else second += line.charAt(i);
+                }
+                n = Integer.parseInt(first);
+                if (second.length() > 0) { m = Integer.parseInt(second); }
+                System.out.println(m+n);
+            }
+        }
     }
 }
