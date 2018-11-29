e861a094590cedc5be0ee9e79c5c873512ccfeab
Scaryman <scaryman86@gmail.com>
2018-11-29 10:18:50 +0300

    result

--- src/repl/Main.java
+++ src/repl/Main.java
@@ -1,7 +1,22 @@
 package repl;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String s = in.nextLine();
+            if (s.equals("/exit")) {
+                System.out.println("Buy!");
+                return;
+            }
+            String[] ary = s.split(" ");
+            if (ary.length == 2) {
+                System.out.println(Integer.parseInt(ary[0]) + Integer.parseInt(ary[1]));
+            } else if (ary.length == 1) {
+                System.out.println(Integer.parseInt(ary[0]));
+            }
+        }
     }
 }
