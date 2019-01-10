de66d5a36006b70739acfcd2e55d27966ee99ce1
Alex <38141304+Alex12367@users.noreply.github.com>
2019-01-10 22:27:18 +0200

    Update Main.java

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,7 +1,47 @@
 package calculator;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+         int sum;
+        String exit = "/exit";
+        String help = "/help";
+        String input;
+
+        while (true) {
+            sum= 0;
+            Scanner sc = new Scanner(System.in);
+            input = sc.nextLine();
+
+
+            if(input.equals(""))
+            continue;
+
+
+
+            if(input.equals(exit))
+            {
+                System.out.println("Bye");
+                break;
+            }
+
+            if(input.equals(help))
+            {
+                System.out.println("The program calculates the sum of numbers");
+                continue;
+            }
+
+
+
+            for (String number : input.split(" ")) {
+                sum += Integer.parseInt(number);
+            }
+            System.out.println(sum);
+
+
+
+        }
+
     }
 }
