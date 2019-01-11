92e96e0ea2ba99966ffa7ea98bd7aa7500e853fb
Hexronimo <leafcat@mail.ru>
2019-01-11 11:06:49 +0300

    Stage #2: The sum of two integers in a loop

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,7 +1,41 @@
 package calculator;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
+class Main {
+
+    static int i;
+
+    public static void main (String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        String line="";
+        i=0;
+        while (!line.equals("/exit")) {
+            line = sc.nextLine();
+            int[] arr = scanning(line);
+            if (!line.equals("/exit")) System.out.println(sum(arr));
+        }
+        System.out.println("Bye!");
+    }
+
+
+    static int sum(int[] arr) {
+        int s=0;
+        for(int j=0;j<i; j++ ){
+            s=arr[j]+s;
+        }
+        return s;
+    }
+
+    static int[] scanning(String line){
+        Scanner scint = new Scanner(line);
+        int[] arr=new int[25];
+        while(scint.hasNextInt()){
+            arr[i] = scint.nextInt();
+            i++;
+        }
+
+        return arr;
     }
 }
