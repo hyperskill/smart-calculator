f99e7363648c569ee4f84915935800d4ee5fd239
superhakeroftheworld <diablahaker2003@gmail.com>
2019-01-29 19:11:13 +0300

    second ex

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,7 +1,22 @@
-package calculator;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
-    }
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		String s = scanner.nextLine();
+		while (!s.equals("/exit")) {
+			if (s.contains(" ")) {
+				String[] nums = new String[2];
+				nums = s.split(" ");
+				System.out.println(Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]));
+			}
+			else if (s.isEmpty()) {
+			}
+			else {
+				System.out.println(s);
+			}
+			s = scanner.nextLine();
+		}
+	
 }
