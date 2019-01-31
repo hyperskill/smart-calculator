990070868868f5943cc5c4b02e6256212eb2f86a
Sensdoo <sensibleprogrammer@gmail.com>
2019-02-01 01:25:35 +0300

    Stage #5: Invalid input handling and changed parseLine method

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Stage #2: The sum of two integers in a loop
 Stage #3: The sum of multiple integers (implemented in previous step, added "help" command)
 
 Stage #4: Addition and subtraction
+
+Stage #5: Invalid input handling
--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,41 +13,61 @@ public class Main {
                 String[] values = parseLine(line);
                 long sum = sum(values);
                 System.out.println(sum);
-            } else {
-                System.out.println("Incorrect input. Try again.");
             }
         }
     }
+
     private static boolean checkUserInput(String input) {
         if ("".equals(input.trim()) || input.isEmpty() || input == null) {
             return false;
         }
 
-        if ("/help".equals(input)) {
-            printHelp();
+        if ('/' == input.charAt(0)) {
+            if ("/help".equals(input)) {
+                printHelp();
+                return false;
+            } else if ("/exit".equals(input)) {
+                System.out.println("Bye!");
+                System.exit(0);
+            } else {
+                System.out.println("Unknown command");
+                return false;
+            }
+        }
+
+        Pattern pattern = Pattern.compile("(^[+-]?\\d+$)|([+-]?\\d*[+-]+\\d+)+");
+        Matcher matcher = pattern.matcher(input.replaceAll("\\s*", ""));
+
+        if (matcher.matches()) {
+            return true;
+        } else {
+            System.out.println("Incorrect input. Try again.");
             return false;
         }
-
-        if ("/exit".equals(input)) {
-            System.out.println("Bye!");
-            System.exit(0);
-        }
-
-        return true;
     }
 
     private static String[] parseLine(String line) {
+        String[] values;
 
-        line = line.replaceAll("-{2}", "+");
-        line = line.replaceAll("[+]{2,}", "+");
-        line = line.replaceAll("[+][-]|[-][+]", "-");
-        line = line.replaceAll("\\s+", "");
-        line = line.replaceAll("[+]", " +").replaceAll("[-]", " -");
-        line = line.replaceAll("^\\s*|\\s*$", "");
+        while (true) {
+            line = line.replaceAll("-{2}", "+");
+            line = line.replaceAll("[+]{2,}", "+");
+            line = line.replaceAll("[+][-]|[-][+]", "-");
 
-        String[] values = line.split(" ");
+            if (line.contains("+-") || line.contains("-+") || line.contains("--")) {
+                continue;
+            }
+
+            line = line.replaceAll("\\s+", "");
+            line = line.replaceAll("[+]", " +").replaceAll("[-]", " -");
+            line = line.replaceAll("^\\s*|\\s*$", "");
+
+            values = line.split(" ");
+            break;
+        }
         return values;
     }
+
     private static long sum(String[] values) {
         if (values.length == 1) {
             return Long.parseLong(values[0]);
@@ -62,6 +84,6 @@ public class Main {
         System.out.println("The program calculates the sum of the numbers.\n" +
                 "Enter the equation using numbers, plus and minus.\n" +
                 "For example, -2 + 3 - 5 + 10\n" +
-                "or \"\\exit\" for closing programm");
+                "or \"\\exit\" for closing program");
     }
 }
