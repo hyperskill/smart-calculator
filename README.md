3515e8a65df5a803d77b2e69ab387f57d66a1c05
Sensdoo <sensibleprogrammer@gmail.com>
2019-01-31 21:39:44 +0300

    Stage #4: Addition and subtraction

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Project: Smart Calculator
 Stage #2: The sum of two integers in a loop
 
 Stage #3: The sum of multiple integers (implemented in previous step, added "help" command)
+
+Stage #4: Addition and subtraction
--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -5,23 +5,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         while (true) {
             String line = scanner.nextLine();
             if (checkUserInput(line)) {
-                parseLine(line);
+                String[] values = parseLine(line);
+                long sum = sum(values);
+                System.out.println(sum);
+            } else {
+                System.out.println("Incorrect input. Try again.");
             }
         }
     }
-
     private static boolean checkUserInput(String input) {
-
         if ("".equals(input.trim()) || input.isEmpty() || input == null) {
             return false;
         }
 
         if ("/help".equals(input)) {
-            System.out.println("The program calculates the sum of numbers");
+            printHelp();
             return false;
         }
 
@@ -33,23 +34,34 @@ public class Main {
         return true;
     }
 
-    private static void parseLine(String line) {
+    private static String[] parseLine(String line) {
+
+        line = line.replaceAll("-{2}", "+");
+        line = line.replaceAll("[+]{2,}", "+");
+        line = line.replaceAll("[+][-]|[-][+]", "-");
+        line = line.replaceAll("\\s+", "");
+        line = line.replaceAll("[+]", " +").replaceAll("[-]", " -");
+        line = line.replaceAll("^\\s*|\\s*$", "");
+
         String[] values = line.split(" ");
-
-        if (values.length == 1) {
-            System.out.println(values[0]);
-        } else {
-            System.out.println(sum(values));
-        }
+        return values;
     }
-
     private static long sum(String[] values) {
-        long result = 0;
+        if (values.length == 1) {
+            return Long.parseLong(values[0]);
+        }
 
+        long result = 0;
         for (int i = 0; i < values.length; i++) {
             result += Long.parseLong(values[i]);
         }
-
         return result;
+    }
+
+    private static void printHelp() {
+        System.out.println("The program calculates the sum of the numbers.\n" +
+                "Enter the equation using numbers, plus and minus.\n" +
+                "For example, -2 + 3 - 5 + 10\n" +
+                "or \"\\exit\" for closing programm");
     }
 }
