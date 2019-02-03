bc2cf741de8b042ef4f4085781474b7535f28070
SashaSmolenkov <antonbobanavt@gmail.com>
2019-02-03 19:41:37 +0300

    MSG

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,7 +1,29 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello world!");
+    public static void main(String[] args)
+    {
+         Scanner scanner = new Scanner(System.in);
+
+         boolean isExit = false;
+
+         while (!isExit) {
+
+             int num1 = scanner.nextInt();
+             int num2 = scanner.nextInt();
+
+             String exit = scanner.nextLine();
+
+             if (exit.endsWith("/exit")) {
+                 isExit = true;
+                 System.out.println("Bye!");
+                 break;
+             }
+
+             System.out.println(num1 + num2);
+
+         }
     }
 }
