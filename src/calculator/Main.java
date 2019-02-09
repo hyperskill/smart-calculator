4a30dfc7b13230c669677a6827e90dc63d3282c6
denderov <denderov@gmail.com>
2019-02-10 01:30:37 +0300

    Main changed.

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,7 +1,33 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+
+        while (!line.equals("/exit")) {
+            if (line.length() > 0) {
+
+                String[] numbers = line.split(" ");
+
+                int result = 0;
+                for (String number:
+                        numbers) {
+                    result += Integer.parseInt(number);
+                }
+
+                System.out.println(result);
+            }
+
+            line = scanner.nextLine();
+        }
+
+        System.out.print("Bye!");
+
     }
+
 }
