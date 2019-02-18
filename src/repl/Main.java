236c72a32884bfb0dead3e4731ed12631000b213
Ratiborus <44792370+Ratiborus@users.noreply.github.com>
2019-02-18 17:01:16 +0300

    Update Main.java

--- src/repl/Main.java
+++ src/repl/Main.java
@@ -1,33 +1,121 @@
-package repl;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Main{
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String exit = "";
-    while (!exit.equalsIgnoreCase("/exit")) {
-      String[] s = br.readLine().split(" ");
-      exit = s[0];
-      if (exit.equalsIgnoreCase("/exit")){
-        System.out.println("Bye!");
-        break;
-      }
-      else if (s.length ==1 && !exit.equalsIgnoreCase("/exit")){
-        System.out.println(s[0]);
-      }
-      else if (s.length > 1) {
-        int a = Integer.parseInt(s[0]);
-        int b = Integer.parseInt(s[1]);
+public class Main {
+    private static boolean cycle = true;
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (cycle){
+            String data = reader.readLine();
 
-        System.out.println(a + b);
-      }
+            String[] strings = patternSet(data);
+            System.out.println("------------------------------------------------------------------------------");
 
-    } 
+            if (strings.length == 1){
+                if (isCommand(strings[0])){
+                    switch (strings[0]){
+                        case "/exit":
+                            cycle = false;
+                            System.out.println("Bye!");
 
+                            break;
+                        case "/help":
+                            System.out.println("------------------------------------------------------------------------------");
+                            System.out.println("Available commands : /help (for helping) ; /exit (for exiting)");
+                            System.out.println("------------------------------------------------------------------------------");
+                            System.out.println("The program supporting the addition \"+\" and subtraction \"-\" operators" +
+                                    "\n The program calculating expressions like these: 4 + 6 - 8, 2 - 3 - 4, and so on.\n\n" +
+                                    "If you has entered several operators following each other,\n" +
+                                    " the program still working (like Java or Python REPL).\n Interpret two adjacent minus signs as a plus. " +
+                                    "\"--\" => \"+\" and \"---*\" => \"-\".");
+                            System.out.println("------------------------------------------------------------------------------");
+                            break;
+                            default:
+                                System.out.println("Uncnown command : " + strings[0]);
+                                break;
 
-  }
-}
+                    }
+                    continue;
+                }else {
+                    if (strings[0].matches("\\+{1}\\d+")){
+                        String s = strings[0].substring(1);
+                        System.out.println(s);
+                        continue;
+                    }else if (strings[0].matches("-{1}\\d+")){
+                        System.out.println(strings[0]);
+                        continue;
+                    }else if (strings[0].matches("[a-zA-Z=]+")||strings[0].matches("\\d+[+=-]")){
+                        System.out.println("Invalid expression");
+                        continue;
+                    }
+                }
+            }
+            
+            if (strings.length == 2){
+                System.out.println("Invalid expression");
+                continue;
+            }
+            long c = Long.parseLong(strings[0]);
+            String symbol = "";
+            for (int i = 1; i < strings.length; i++) {
+                if (strings[i].matches("([-+])")){
+                    symbol = strings[i];
+                    continue;
+                }
+                c = simpleExpression(c, symbol, Long.parseLong(strings[i]));
+            }
+            System.out.println(c);
+
+            
+            
+
+        }
+    }
+
+    public static String[] patternSet(String s){
+        String temp;
+
+        Pattern pattern = Pattern.compile("\\s+");
+        Matcher matcher = pattern.matcher(s);
+        temp = matcher.replaceAll(" ");
+
+        pattern = Pattern.compile("\\s{1}-{1}-{1}\\s{1}");
+        matcher = pattern.matcher(temp);
+        temp = matcher.replaceAll(" + ");
+
+        pattern = Pattern.compile("\\s{1}-{3,}\\s{1}");
+        matcher = pattern.matcher(temp);
+        temp = matcher.replaceAll(" - ");
+
+        pattern = Pattern.compile("\\s{1}\\+{2,}\\s{1}");
+        matcher = pattern.matcher(temp);
+        temp = matcher.replaceAll(" + ");
+
+        String[] strings = temp.split("\\s");
+        return strings;
+
+    }
+    public  static boolean isCommand(String s){
+        if (s.matches("/{1}.*")){
+            return true;
+        }else return false;
+
+    }
+
+    public static long simpleExpression(long a, String s, long b){
+        long answer = 0;
+        switch (s){
+            case "+":
+                answer = a + b;
+                break;
+            case "-":
+                answer = a - b;
+                break;
+        }
+       return answer;
+    }
 }
