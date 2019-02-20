226f8cef5491fb8d8b603a899500575f054830ea
denderov <denderov@gmail.com>
2019-02-20 17:35:56 +0300

    Smart calculator.

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -6,31 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
+        SmartCalculator.runCalculator();
 
-        while (!line.equals("/exit")) {
-
-            if (line.equals("/help")) {
-                System.out.println("Any help here");
-            }
-            else if (line.length() > 0) {
-
-                String[] numbers = line.split(" ");
-
-                int result = 0;
-                for (String number:
-                        numbers) {
-                    result += Integer.parseInt(number);
-                }
-
-                System.out.println(result);
-            }
-
-            line = scanner.nextLine();
-        }
-
-        System.out.print("Bye!");
+//        Scanner scanner = new Scanner(System.in);
+//        String line = scanner.nextLine();
+//
+//        while (!line.equals("/exit")) {
+//
+//            if (line.equals("/help")) {
+//                System.out.println("Any help here");
+//            }
+//            else if (line.length() > 0) {
+//
+//                String[] numbers = line.split(" ");
+//
+//                int result = 0;
+//                for (String number:
+//                        numbers) {
+//                    result += Integer.parseInt(number);
+//                }
+//
+//                System.out.println(result);
+//            }
+//
+//            line = scanner.nextLine();
+//        }
+//
+//        System.out.print("Bye!");
 
     }
 
---
+++ src/calculator/SmartCalculator.java
@@ -0,0 +1,58 @@
+package calculator;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class SmartCalculator {
+
+    static void runCalculator() {
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+
+        while (!line.equals("/exit")) {
+
+            if (line.equals("/help")) {
+                System.out.println("Any help here");
+            }
+            else if (line.length() > 0) {
+
+                Matcher matcher = Pattern.compile("(^|[ +-])+\\d+").matcher(line);
+
+                int result = 0;
+                while (matcher.find()) {
+                    result += getNumberFromString(matcher.group());
+                }
+
+                System.out.println(result);
+
+            }
+
+            line = scanner.nextLine();
+        }
+
+        System.out.print("Bye!");
+
+    }
+
+    private static int getNumberFromString(String group) {
+
+        char[] charArray = group.toCharArray();
+        boolean isNegative = false;
+        int charPositionOfZero = (int)'0';
+        int result = 0;
+
+        for (char symbol:
+             charArray) {
+            if (symbol == '-') isNegative = !isNegative;
+            if (symbol >= charPositionOfZero&& symbol <= charPositionOfZero+10) {
+                result = result*10+(int)symbol-charPositionOfZero;
+            }
+            if (isNegative) result = -result;
+        }
+
+        return result;
+    }
+
+}
