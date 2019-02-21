8c698c0b3f166e520284aff4a060571459c14049
superhakeroftheworld <diablahaker2003@gmail.com>
2019-02-21 18:42:32 +0300

    пятый степ задачи, наконец-то с регулярками

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,29 +1,12 @@
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-	public static void main(String args[]) {
-		Scanner scanner = new Scanner(System.in);
-		String s = scanner.nextLine();
-		while (!s.equals("/exit")) {
-			if(s.isEmpty()) {}
-			else {
-				if(s.equals("/help")) {
-					System.out.println("The program calculates the sum of numbers");
-				}
-				else {
-					int result = 0;
-					String[] nums = s.split(" ");
-					for(int i = 0; i < nums.length; i++) {
-						result += Integer.valueOf(nums[i]);
-					}
-					if(nums.length > 0) {
-						System.out.println(result);
-					}
-				}
-			}
-			s = scanner.nextLine();
-		}
-	}
-	
+public class Main
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+	linePreprocessing input = new linePreprocessing();
+	input.preprocessing();
+    }
 }
