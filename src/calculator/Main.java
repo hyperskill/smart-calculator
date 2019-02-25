1f9349640475466c7b77726fe60465cf526c1e4a
SashaSmolenkov <antonbobanavt@gmail.com>
2019-02-25 20:12:38 +0300

    MSG

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -3,27 +3,30 @@ package calculator;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args)
-    {
-         Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        String curStr = "";
 
-         boolean isExit = false;
+        while (true) {
+            curStr = scanner.nextLine();
+            if (curStr.equals("/help")) {
+                System.out.println("The program calculates the sum of numbers");
 
-         while (!isExit) {
+            } else if (curStr.equals("/exit")) {
+                System.out.println("Bye!");
+                return;
 
-             int num1 = scanner.nextInt();
-             int num2 = scanner.nextInt();
-
-             String exit = scanner.nextLine();
-
-             if (exit.endsWith("/exit")) {
-                 isExit = true;
-                 System.out.println("Bye!");
-                 break;
-             }
-
-             System.out.println(num1 + num2);
-
-         }
+            } else {
+                if (!("".equals(curStr))) {
+                    String[] nums = curStr.split(" ");
+                    for (String num : nums) {
+                        sum += Integer.valueOf(num);
+                    }
+                }
+                System.out.println(sum);
+                sum = 0;
+            }
+        }
     }
 }
