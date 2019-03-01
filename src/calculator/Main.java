bc999b209c056a05e546dd278e38977703156a8b
SashaSmolenkov <47061258+SashaSmolenkov@users.noreply.github.com>
2019-03-01 18:24:17 +0300

    Update Main.java to the answer
    Answer for 3th stage

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        String curStr = "";
+
+        while (true) {
+            curStr = scanner.nextLine();
+            if (curStr.equals("/help")) {
+                System.out.println("The program calculates the sum of numbers");
+
+            } else if (curStr.equals("/exit")) {
+                System.out.println("Bye!");
+                return;
+
+            } else {
+                if (!("".equals(curStr))) {
+                    String[] nums = curStr.split(" ");
+                    for (String num : nums) {
+                        sum += Integer.valueOf(num);
+                    }
+                }
+                System.out.println(sum);
+                sum = 0;
+            }
+        }
     }
 }
