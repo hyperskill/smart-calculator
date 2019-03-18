c0869d2f2442eb329c2e81061e0bb98a399b6c7b
denderov <denderov@gmail.com>
2019-03-18 18:16:50 +0300

    Smart calculator. Refactoring of runCalculator. Case version.

--- src/calculator/SmartCalculator.java
+++ src/calculator/SmartCalculator.java
@@ -8,11 +8,10 @@ import java.util.regex.Pattern;
 
 class SmartCalculator {
 
-    private static Map<Character, Integer> variables;
+    private enum Status {ORDINAL_EXPRESSION,ASSIGNMENT,VARIABLE,EMPTY_LINE,COMMAND,
+        INVALID_EXPRESSION,INVALID_IDENTIFIER,INVALID_VALUE,INVALID_ASSIGNMENT}
 
-    public SmartCalculator() {
-        variables = new HashMap<>();
-    }
+    Map<Character, Integer> variables = new HashMap<>();
 
     static void runCalculator() {
 
@@ -21,11 +20,7 @@ class SmartCalculator {
 
         while (!line.equals("/exit")) {
 
-            try {
-                parseLine(line);
-            } catch (IllegalArgumentException e) {
-                System.out.println("Invalid expression");
-            }
+            parseLine(line);
 
             line = scanner.nextLine();
 
@@ -36,29 +31,117 @@ class SmartCalculator {
     }
 
     private static void parseLine(String line) {
-        if (isCommand(line)) {
 
-            executeCommand(line);
+        Status lineStatus = computeLineStatus(line);
 
-        } else if (isAssignment(line)) {
+        switch (lineStatus) {
+            case ORDINAL_EXPRESSION:
+                processExpression(line);
+                break;
+            case ASSIGNMENT:
+                processAssingment(line);
+                break;
+            case VARIABLE:
+                printVariable(line);
+                break;
+            case COMMAND:
+                executeCommand(line);
+                break;
+            case INVALID_VALUE:
+                System.out.println("Invalid value");
+                break;
+            case INVALID_ASSIGNMENT:
+                System.out.println("Invalid assignment");
+                break;
+            case INVALID_EXPRESSION:
+                System.out.println("Invalid expression");
+                break;
+            case INVALID_IDENTIFIER:
+                System.out.println("Invalid value");
+                break;
+            case EMPTY_LINE:
+                break;
+        }
 
-        } else if (isInvalidExpression(line)) {
+    }
 
-            throw new IllegalArgumentException("Invalid expression");
+    private static Status computeLineStatus(String line) {
 
-        } else if (line.length() > 0) {
+        if (line.length() == 0) return Status.EMPTY_LINE;
 
-            Matcher matcher = Pattern.compile("(^|[ +-])+\\d+").matcher(line);
+        if (isAssignment(line)) return Status.ASSIGNMENT;
 
-            int result = 0;
-            while (matcher.find()) {
+        if (isVariable(line)) return  Status.VARIABLE;
+
+        if (isCommand(line)) return Status.COMMAND;
+
+        if (isOrdinalExpression(line)) return Status.ORDINAL_EXPRESSION;
+
+
+
+
+        return Status.INVALID_EXPRESSION;
+
+    }
+
+    private static boolean isVariable(String line) {
+
+        Matcher matcher = Pattern.compile("(^|[ +-])+([a-zA-z])+\\s+$").matcher(line);
+
+        return matcher.find();
+
+    }
+
+    private static boolean isOrdinalExpression(String line) {
+
+        Matcher matcher = Pattern.compile("((^|[ +-])+(\\d|[a-zA-z])+)+$").matcher(line);
+
+        return matcher.find();
+
+    }
+
+    private static void printVariable(String line) {
+
+        System.out.println(getNumberFromVariable(line));
+
+    }
+
+    private static void processAssingment(String line) {
+
+
+
+    }
+
+    private static void processExpression(String line) {
+
+        Matcher matcher = Pattern.compile("(^|[ +-])+(\\d|[a-zA-z])+").matcher(line);
+
+        int result = 0;
+
+        while (matcher.find()) {
+
+            if (matcher.group().matches("(^|[ +-])+(\\d)+")) {
+
                 result += getNumberFromString(matcher.group());
+
+            } else {
+
+                result += getNumberFromVariable(matcher.group());
+
             }
 
-            System.out.println(result);
-
         }
+
+        System.out.println(result);
+
     }
+
+    private static int getNumberFromVariable(String group) {
+
+        return  0;
+
+    }
+
 
     private static boolean isAssignment(String line) {
         return line.contains("=");
@@ -74,7 +157,7 @@ class SmartCalculator {
     }
 
     private static boolean isCommand(String line) {
-        Matcher matcher = Pattern.compile("^\\/\\w+$").matcher(line);
+        Matcher matcher = Pattern.compile("^/\\w+$").matcher(line);
         return matcher.find();
     }
 
@@ -95,16 +178,6 @@ class SmartCalculator {
 
         if (isNegative) result = -result;
         return result;
-    }
-
-    private static boolean isInvalidExpression(String expression) {
-        Matcher matcher = Pattern.compile("^\\s*[+-]*\\s*\\d+(\\s*[+-]+\\s*+\\d+)*\\s*$").matcher(expression);
-        return !matcher.find();
-    }
-
-    private static boolean isUnknownCommand(String command) {
-        Matcher matcher = Pattern.compile("^\\/(?!help$|exit$)\\w*").matcher(command);
-        return matcher.find();
     }
 
 }
